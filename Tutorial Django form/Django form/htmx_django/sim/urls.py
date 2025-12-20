3a7a107e62d841183c0411c367b94c7ebbada276
josetaka79 <josetaka79@gmail.com>
2025-12-20 20:07:09 +0200

    Delete Tutorial Django form/Django form/htmx_django directory

--- Tutorial Django form/Django form/htmx_django/sim/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.example, name="home"),
-    path("example/", views.example, name="example"),
-    path("sample-post/", views.sample_post, name="sample-post"),
-]
-
